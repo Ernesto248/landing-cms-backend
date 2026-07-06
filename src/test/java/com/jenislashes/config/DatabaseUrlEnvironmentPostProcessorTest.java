7773d072a9961270f000f699d243189c3a5fc572
Ernesto248 <ernestoleonard8@gmail.com>
2026-07-06 10:16:23 -0600

    Register database URL normalizer for Spring Boot

--- src/test/java/com/jenislashes/config/DatabaseUrlEnvironmentPostProcessorTest.java
+++ src/test/java/com/jenislashes/config/DatabaseUrlEnvironmentPostProcessorTest.java
@@ -1,6 +1,7 @@
 package com.jenislashes.config;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.mock.env.MockEnvironment;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -30,5 +31,18 @@ class DatabaseUrlEnvironmentPostProcessorTest {
                 .isEqualTo("jdbc:postgresql://postgres.railway.internal:5432/railway");
         assertThat(normalized.username()).isEqualTo("postgres");
         assertThat(normalized.password()).isEqualTo("password");
+    }
+
+    @Test
+    void postProcessEnvironment_shouldExposeJdbcDatasourceProperties() {
+        MockEnvironment environment = new MockEnvironment()
+                .withProperty("DATABASE_URL", "postgres://postgres:password@postgres.railway.internal:5432/railway");
+
+        new DatabaseUrlEnvironmentPostProcessor().postProcessEnvironment(environment, null);
+
+        assertThat(environment.getProperty("spring.datasource.url"))
+                .isEqualTo("jdbc:postgresql://postgres.railway.internal:5432/railway");
+        assertThat(environment.getProperty("spring.datasource.username")).isEqualTo("postgres");
+        assertThat(environment.getProperty("spring.datasource.password")).isEqualTo("password");
     }
 }
